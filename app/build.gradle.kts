d6cc250edfbd64ae5e47fea065ff5b58ca84a1f1
Rudi Timmermans <rudi.timmermans@mailfence.com>
2025-03-25 06:40:53 +0100

    Update to correct fork version code with lasted Dutch translations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.lucky.silence"
         minSdk = 29
         targetSdk = 34
-        versionCode = 50
-        versionName = "1.11.6"
+        versionCode = 51
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
